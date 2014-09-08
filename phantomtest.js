da5c6df0ef1ba9e726cf9c3fc564505276d90f48
Accalia de Elementia <accalia.de.elementia@gmail.com>
2014-09-08 16:39:18 +0000

    remove test file

--- phantomtest.js
+++
@@ -1,8 +0,0 @@
-console.log('Loading a web page');
-var page = require('webpage').create();
-var url = 'http://what.thedailywtf.com/';
-page.open(url, function (status) {
-  //Page is loaded!
-  console.log(status);
-  phantom.exit();
-});
