8977418e11ee11d320179c58eefaae6c0c47ff8f
Accalia de Elementia <accalia.de.elementia@gmail.com>
2014-09-08 16:39:01 +0000

    switch phantomjs plugin to one that actually works. login attempted, but not successfull?

--- puppet.js
+++ puppet.js
@@ -1,21 +1,30 @@
 var async = require('async'),
-    phantom = require('node-phantom');
+    phantom = require('phantom');
+
+
+/*
+phantom.create('--disk-cache=true', '--ignore-ssl-errors=true',
+    '--load-images=false','--max-disk-cache-size=512',
+    function (ph){
+    ph.createPage(function(page){
+        page.open('http://what.thedailywtf.com/',function(status){
+            console.log(arguments);
+            ph.exit();
+        });
+    });
+})
+*/
 
 function create(cb){
-    console.log(0);
-    phantom.create(cb,{
-        'disk-cache': true,
-        'ignore-ssl-errors': true,
-        'load-images': false,
-        'max-disk-cache-size': 512
-    })
+    phantom.create('--disk-cache=true', '--ignore-ssl-errors=true',
+        '--load-images=false','--max-disk-cache-size=512', cb);
+
 }
 
 function auth(page, username, password, cb){
     async.series([
         function(cb){
-            console.log(3);
-            page.open('http://what.thedailywtf.com/',cb);
+            page.open('http://what.thedailywtf.com/',function(){cb();});
         },function(cb){
             console.log(4);
             page.evaluate(function(username, password){
@@ -23,23 +32,23 @@ function auth(page, username, password, cb){
                 $('#login-account-name').focus().val(username).blur().change();
                 $('#login-account-password').focus().val(password).blur().change();
                 $('div.modal-footer button.btn-primary').click();
-            }, username, password);
-            setTimeout(cb,1000);
+            }, function(val){cb(null,val)}, username, password);
         },function(cb){
             console.log(5);
-            console.log(page.evaluate(function(){
-                return document.querySelector('div.current-username').innerText;
-            }));
-            cb();
+                setTimeout(function() {page.evaluate(function(){
+                    return document.querySelector('div.current-username').innerText;
+                },function(val){console.log(val);cb()});
+            }, 1000);
         }],
         function(){
             console.log(6);
             (cb || function(){})()
         });
 }
-create(function(err, ph){
+
+create(function(ph){
     console.log(1);
-    ph.createPage(function(err,page){
+    ph.createPage(function(page){
         console.log(2);
         auth(page, 'sockbot', 'sockbot', function(){
             ph.exit()
