8bf9acbf4890c906a543b35c691e4dc242da9874
Accalia de Elementia <accalia.de.elementia@gmail.com>
2014-09-09 02:23:22 +0000

    make a post successfully. currently hardcoded to a topic. i'll want to fix that soon

--- puppet.js
+++ puppet.js
@@ -2,14 +2,22 @@ var async = require('async'),
     request = require('request'),
     $ = require('cheerio'),
     jar = request.jar(),
+    CSRF,
     browser = request.defaults({jar:jar,headers:{'X-Requested-With':'XMLHttpRequest','User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.103 Safari/537.36'}});
 
+function getCSRF(request, callback){
+    request.get('http://what.thedailywtf.com/session/csrf?_'+(new Date()*1),function(a,b,c){
+        try{
+            CSRF = JSON.parse(c).csrf;
+        }catch(e){}
+        callback(null, CSRF);
+    });
+}
 
 function auth(request, callback){
-    request.get('http://what.thedailywtf.com/session/csrf?_'+(new Date()*1),function(a,b,c){
-        console.log(c);
-        var obj = JSON.parse(c);
-        request.post('http://what.thedailywtf.com/session', {headers:{'X-CSRF-Token':obj.csrf},form:{login:'sockbot',password:'sockbotsockbot'}}, function(){
+    getCSRF(request,function(e,csrf){
+        console.log(csrf);
+        request.post('http://what.thedailywtf.com/session', {headers:{'X-CSRF-Token':csrf},form:{login:'sockbot',password:'sockbotsockbot'}}, function(){
             request.post('http://what.thedailywtf.com/login', {form:{username:'sockbot',password:'sockbotsockbot',redirect:'http://what.thedailywtf.com/'}}, function(){
                 //request('http://what.thedailywtf.com/users/sockbot/activity.json',function(a,b,c){
                 //    console.log(c);
@@ -19,4 +27,24 @@ function auth(request, callback){
         });
     });
 }
-auth(browser,function(){});
+
+function post(request, callback){
+    getCSRF(request, function(e, csrf){
+        request.post('http://what.thedailywtf.com/posts',{
+            headers:{'X-CSRF-Token':csrf}, 
+            form:{
+                raw:'This is a post from an automated bot. Did it work?',
+                topic_id:3031,
+                reply_to_post_number:'',
+                category:3,
+                archetype:'regular',
+                auto_close_time:'' }},
+            function(a,b,c){
+                console.log(arguments);
+                callback();
+            })
+    })
+
+}
+
+auth(browser,function(){post(browser,function(){})});
