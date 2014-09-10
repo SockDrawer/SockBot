7966936a8e5a62182dee52a43c57e9ba052f4134
Accalia de Elementia <accalia.de.elementia@gmail.com>
2014-09-10 01:02:43 +0000

    add the ability to read a page of a topic.

--- SockBot.js
+++ SockBot.js
@@ -1,69 +1,81 @@
-/*jslint node: true, indent: 4, unparam: true */
+/*jslint node: true, indent: 4, unparam: true, sloppy: true,  */
 
-(function () {
-    'use strict';
-    var async = require('async'),
-        request = require('request'),
-        jar = request.jar(),
-        browser = request.defaults({
-            jar: jar,
-            headers: {
-                'X-Requested-With': 'XMLHttpRequest',
-                'User-Agent': 'SockAdept 1.0.0'
-            }
-        });
-
-    function getContent(browser, url, callback) {
-        // Just in case I need to start wrapping things around my gets as well
-        browser.get(url, callback);
-    }
-
-    function postMessage(browser, url, form, callback) {
-        browser.get('http://what.thedailywtf.com/session/csrf.json',
-            function (a, b, c) {
-                var csrf = '';
-                try {
-                    csrf = JSON.parse(c).csrf;
-                } catch (e) {
-                    callback(e);
-                }
-                browser.post(url, {
-                    headers: {
-                        'X-CSRF-Token': csrf
-                    },
-                    form: form
-                }, callback);
-            });
-    }
-
-    function auth(request, username, password, callback) {
-        async.waterfall([
-
-            function (cb) {
-                postMessage(request, 'http://what.thedailywtf.com/session', {
-                    login: username,
-                    password: password
-                }, cb);
-            },
-            function (req, body, cb) {
-                postMessage(request, 'http://what.thedailywtf.com/login', {
-                    username: username,
-                    password: password,
-                    redirect: 'http://what.thedailywtf.com/'
-                }, cb);
-            },
-            function (req, body, cb) {
-                getContent(request,
-                    'http://what.thedailywtf.com/users/sockbot/activity.json',
-                    function (a, b, c) {
-                        console.log(c);
-                        cb();
-                    });
-            }
-        ], callback);
-    }
-
-    auth(browser, 'sockbot', 'sockbotsockbot', function () {
-        console.log('authenticated?!');
+var async = require('async'),
+    request = require('request'),
+    jar = request.jar(),
+    browser = request.defaults({
+        jar: jar,
+        headers: {
+            'X-Requested-With': 'XMLHttpRequest',
+            'User-Agent': 'SockAdept 1.0.0'
+        }
     });
-}());
+
+function getContent(url, callback) {
+    browser.get('http://what.thedailywtf.com/' + url, callback);
+}
+
+function postMessage(url, form, callback) {
+    browser.get('http://what.thedailywtf.com/session/csrf.json',
+        function (a, b, c) {
+            var csrf = '';
+            try {
+                csrf = JSON.parse(c).csrf;
+            } catch (e) {
+                callback(e);
+            }
+            browser.post('http://what.thedailywtf.com/' + url, {
+                headers: {
+                    'X-CSRF-Token': csrf
+                },
+                form: form
+            }, callback);
+        });
+}
+
+function auth(username, password, callback) {
+    async.waterfall([
+
+        function (cb) {
+            postMessage('session', {
+                login: username,
+                password: password
+            }, cb);
+        },
+        function (req, body, cb) {
+            // Not needed for authentication but registers the login
+            postMessage('login', {
+                username: username,
+                password: password,
+                redirect: 'http://what.thedailywtf.com/'
+            }, cb);
+        }
+    ], callback);
+}
+
+
+function readTopic(topic_id, start_post, callback) {
+    getContent('t/' + topic_id + '/' + start_post + '.json', function (err, req, contents) {
+        var stream = JSON.parse(contents);
+        async.eachSeries(stream.post_stream.posts, function (x, cb) {
+            var form = {
+                'topic_id': topic_id,
+                'topic_time': 1000 // msecs passed on topic (We're pretty sure)
+            };
+            form['timings[' + x.post_number + ']'] = 1000; // msecs passed on post (same)
+            postMessage('topics/timings', form, function () {
+                setTimeout(cb, 500); // Rate limit these to better sout the occasion
+            });
+        }, function () {
+            callback(null, stream.post_stream.posts.length);
+        });
+    });
+}
+
+auth('sockbot', 'sockbotsockbot', function () {
+    console.log('authenticated?!');
+
+    readTopic(1000, 1, function (err, posts) {
+        console.log(posts);
+    });
+});
--- puppet.js
+++ puppet.js
@@ -3,20 +3,24 @@ var async = require('async'),
     $ = require('cheerio'),
     jar = request.jar(),
     CSRF,
-    browser = request.defaults({jar:jar,headers:{'X-Requested-With':'XMLHttpRequest','User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.103 Safari/537.36'}});
+    browser = request.defaults(
+    {
+        jar: jar,
+        headers:
+        {
+            'X-Requested-With': 'XMLHttpRequest',
+            'User-Agent': 'SockAdept 1.0.0'
+        }
+    });
 
 function getCSRF(request, callback){
-    request.get('http://what.thedailywtf.com/session/csrf?_'+(new Date()*1),function(a,b,c){
-        try{
-            CSRF = JSON.parse(c).csrf;
-        }catch(e){}
-        callback(null, CSRF);
+    request.get('http://what.thedailywtf.com/session/csrf.json',function(a,b,c){
+        callback(null, JSON.parse(c).csrf);
     });
 }
 
 function auth(request, callback){
     getCSRF(request,function(e,csrf){
-        console.log(csrf);
         request.post('http://what.thedailywtf.com/session', {headers:{'X-CSRF-Token':csrf},form:{login:'sockbot',password:'sockbotsockbot'}}, function(){
             request.post('http://what.thedailywtf.com/login', {form:{username:'sockbot',password:'sockbotsockbot',redirect:'http://what.thedailywtf.com/'}}, function(){
                 //request('http://what.thedailywtf.com/users/sockbot/activity.json',function(a,b,c){
@@ -33,7 +37,7 @@ function post(request, callback){
         request.post('http://what.thedailywtf.com/posts',{
             headers:{'X-CSRF-Token':csrf}, 
             form:{
-                raw:'This is a post from an automated bot. Did it work?',
+                raw:'This is a post from an automated bot performing another test. Did it work?',
                 topic_id:3031,
                 reply_to_post_number:'',
                 category:3,
