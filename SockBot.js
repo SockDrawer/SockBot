dcfcb800ee05d4b5953194c0076fd57cb8a0aac4
Accalia de Elementia <accalia.de.elementia@gmail.com>
2014-09-27 00:59:37 +0000

    add readify of historical documents

--- SockBot.js
+++ SockBot.js
@@ -3,31 +3,15 @@
 var async = require('async'),
     browser = require('./browser'),
     likes = require('./likes'),
+    reader = require('./reader'),
     config = require('./configuration').configuration;
 
 
-
-function readTopic(topic_id, start_post, callback) {
-    browser.getContent('t/' + topic_id + '/' + start_post + '.json', function (err, req, contents) {
-        var stream = JSON.parse(contents);
-        async.eachSeries(stream.post_stream.posts, function (x, cb) {
-            var form = {
-                'topic_id': topic_id,
-                'topic_time': 1000 // msecs passed on topic (We're pretty sure)
-            };
-            form['timings[' + x.post_number + ']'] = 1000; // msecs passed on post (same)
-            browser.postMessage('topics/timings', form, function () {
-                setTimeout(cb, 500); // Rate limit these to better sout the occasion
-            });
-        }, function () {
-            callback(null, stream.post_stream.posts.length);
-        });
-    });
-}
-
 browser.auth(config.username, config.password, function () {
-    console.log(config);
     if (config.likeBinge) {
         likes.likeThread(browser, config.likeBingeTopics);
+    }
+    if (config.readify){
+        reader.readAllTheThings(browser);
     }
 });
---
+++ reader.js
@@ -0,0 +1,96 @@
+/*jslint node: true, indent: 4, unparam: true  */
+(function () {
+    'use strict';
+    var async = require('async'),
+        config = require('./configuration').configuration,
+        laterReads = [];
+
+    function readAllTheThings(browser) {
+        var next = '/latest.json?ascending=true&order=activity&no_definitions=true';
+        async.whilst(
+            function () {
+                return !!next;
+            },
+            function (cb) {
+                getTopicsPage(browser, next, function (topic_list) {
+                    next = topic_list.next;
+                    async.eachSeries(topic_list.topics, function (topic, seriescb) {
+                            console.log('Reading topic ' + topic.id + ' ' + topic.slug);
+                            var i = 1,
+                                max = 2;
+                            async.whilst(function () {
+                                    return i < max;
+                                },
+                                function (whilstcb) {
+                                    getTopicPage(browser, topic.id, i, function (page) {
+                                        i = page.last + 1;
+                                        max = page.posts;
+                                        readTopicPage(browser, page, whilstcb);
+                                    });
+                                }, seriescb);
+                        },
+                        cb);
+                });
+            });
+    }
+    exports.readAllTheThings = readAllTheThings;
+
+    function readTopicPage(browser, topic, callback) {
+        var cutoff = (new Date().getTime()) - config.readifyWait,
+            form = {
+                'topic_id': topic.id,
+                'topic_time': Math.floor(4995 * topic.unread.length / 3) // msecs passed on topic (We're pretty sure)
+            };
+        topic.unread.filter(function (p) {
+            return p.posted < cutoff;
+        }).map(function (p) {
+            form['timings[' + p.id + ']'] = 4995; // msecs passed on post (same)
+        });
+        topic.unread.filter(function (p) {
+            return p.posted >= cutoff;
+        }).map(function (p) {
+            p.topic_id = topic.id;
+            laterReads.push(p);
+        });
+
+        browser.postMessage('topics/timings', form, function () {
+            setTimeout(callback, 50); // Rate limit these to better sout the occasion
+        });
+    }
+
+    function getTopicsPage(browser, url, callback) {
+        browser.getContent(url, function (a, b, c) {
+            callback({
+                next: c.topic_list.more_topics_url,
+                topics: c.topic_list.topics.map(function (o) {
+                    return {
+                        id: o.id,
+                        slug: o.slug
+                    };
+                })
+            });
+        });
+    }
+
+    function getTopicPage(browser, topic, post, callback) {
+        browser.getContent('/t/' + topic + '/' + post + '.json', function (a, b, c) {
+            var posts = c.post_stream.posts;
+            callback({
+                start: c.last_read_post_number,
+                last: posts[posts.length - 1].post_number,
+                posts: c.highest_post_number,
+                id: c.id,
+                slug: c.slug,
+                unread: posts.filter(function (p) {
+                    return !p.read;
+                }).map(function (p) {
+                    return {
+                        id: p.post_number,
+                        posted: Date.parse(p.created_at)
+                    };
+                })
+            });
+        });
+    }
+
+}());
