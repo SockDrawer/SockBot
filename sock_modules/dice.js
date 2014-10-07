8def0c212549b123c984b8e76221d725760bb062
Accalia de Elementia <accalia.de.elementia@gmail.com>
2014-10-07 16:48:03 +0000

    allow reroll critical hists

--- sock_modules/dice.js
+++ sock_modules/dice.js
@@ -4,21 +4,63 @@
 
     var async = require('async'),
         xRegExp = require('xregexp').XRegExp,
-        matcher = '\\b(?<num>\\d+)(d(?<sides>\\d+)|(\\s?(?<method>S|SS|Scion|W|WW|Wolf|M|MM|Mage|F|FF|Fudge|Fate|C|CC|Coin)))(t(?<target>\\d+))?(?<options>[pr]+)?\\b',
+        matcher = '(^|\\s)(?<num>-?\\d+)(d(?<sides>-?\\d+)|(\\s?(?<method>S|SS|Scion|W|WW|Wolf|M|MM|Mage|F|FF|Fudge|Fate|C|CC|Coin)))(t(?<target>\\d+))?(?<options>[pr]+)?($|\\s)',
         rmatcher = xRegExp(matcher, 'i'),
         m_browser,
         m_config;
 
-    function getDiceFromServer(num, sides, callback) {
-        m_browser.getContent('http://www.random.org/integers/?num=' + num + '&min=1&max=' + sides + '&col=1&base=10&format=plain&rnd=new', function (err, res, data) {
-            if (err || res >= 300) {
-                callback();
-            } else {
-                callback(data.trim().split("\n").map(function (v) {
-                    return parseInt(v, 10);
-                }));
+    function getDiceFromServer(num, sides, rerollGreater, callback) {
+        var factor = sides < 0 ? -1 : 1,
+            results = [],
+            sum = 0,
+            toRoll;
+        rerollGreater = Math.abs(rerollGreater);
+        rerollGreater = isNaN(rerollGreater) ? Number.Infinity : rerollGreater;
+        if (num < 0) {
+            num *= -1;
+        }
+        if (sides < 0) {
+            sides *= -1;
+        }
+        toRoll = num;
+        async.whilst(
+            function () {
+                return toRoll > 0;
+            },
+            function (next) {
+                if (results.length >= (m_config.diceMaxReRolls || 20)) {
+                    results.push(['Too many Rerolls. Stopping.']);
+                    next(true);
+                    return;
+                }
+                m_browser.getContent('http://www.random.org/integers/?num=' + toRoll + '&min=1&max=' + sides + '&col=1&base=10&format=plain&rnd=new', function (err, res, data) {
+
+                    var dice;
+                    if (err || res >= 300) {
+                        next(true);
+                    } else {
+                        dice = data.trim().split("\n").map(function (v) {
+                            return parseInt(v, 10);
+                        });
+                        toRoll = dice.reduce(function (i, v) {
+                            return i + (v >= rerollGreater ? 1 : 0);
+                        }, 0);
+                        dice = dice.map(function (v) {
+                            return v * factor;
+                        });
+                        sum = dice.reduce(function (i, v) {
+                            return i + v;
+                        }, sum);
+                        results.push(dice);
+                        next();
+                    }
+                });
+            },
+            function () {
+                callback(sum, results);
             }
-        });
+        );
+
     }
 
     function getError() {
@@ -33,6 +75,8 @@
             reducer = function reducer(i, o) {
                 return i + o === 1 ? 1 : 0;
             };
+        num = Math.abs(num);
+        sides = Math.abs(sides);
         while (ones > 0) {
             dice = [];
             for (i = 0; i < num; i += 1) {
@@ -44,9 +88,12 @@
         return ct;
     }
 
+
+
     function rollXDice(match, callback) {
         var num = parseInt(match.num, 10),
             sides = parseInt(match.sides, 10),
+            crit,
             result = 'Rolling ' + num + 'd' + sides + ': ';
 
         if (isNaN(num) || isNaN(sides)) {
@@ -64,15 +111,21 @@
         if (match.options && match.options.toLowerCase().indexOf('p') >= 0) {
             result += 'Prerolling ' + prerollDice(num, sides) + ' times: ';
         }
-        getDiceFromServer(num, sides, function (dice) {
-            var sum = dice.reduce(function (i, v) {
-                return i + v;
-            }, 0);
-            result += dice.join(', ');
+        if (match.options && match.options.toLowerCase().indexOf('r') >= 0) {
+            crit = sides;
+        }
+        getDiceFromServer(num, sides, crit, function (sum, dice) {
+            result += dice.shift().join(', ');
+            if (dice.length > 0) {
+                dice.forEach(function (d) {
+                    result += '\nRerolling ' + d.length + ' Crits:' + d.join(', ');
+                });
+                result += '\n';
+            }
             if (isNaN(sum)) {
                 result += ' ' + getError();
-            } else if (num > 1) {
-                result += ' Sum: ' + sum;
+            } else if (Math.abs(num) > 1) {
+                result += ' Sum: ' + sum * (num < 0 ? -1 : 1);
             }
             callback(result);
         });
