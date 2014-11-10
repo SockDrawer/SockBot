714493b90916dfa9e10484ebe4b726f7a0d112ba
Accalia <accalia.de.elementia@gmail.com>
2014-11-10 08:05:05 -0500

    remove self executing function as unecessary

--- sock_modules/anonymize.js
+++ sock_modules/anonymize.js
@@ -1,52 +1,49 @@
 /*jslint node: true, indent: 4 */
-(function () {
-    'use strict';
-    var xRegExp = require('xregexp').XRegExp;
-    var discourse,
-        conf,
-        rQuote = xRegExp('\\[quote.*post:(?<post_number>\\d+).*' +
-            'topic:(?<topic_id>\\d+)');
+'use strict';
+var xRegExp = require('xregexp').XRegExp;
+var discourse,
+    conf,
+    rQuote = xRegExp('\\[quote.*post:(?<post_number>\\d+).*' +
+        'topic:(?<topic_id>\\d+)');
 
-    exports.description = 'Anonymize replies';
+exports.description = 'Anonymize replies';
 
-    exports.configuration = {
-        enabled: false
-    };
+exports.configuration = {
+    enabled: false
+};
 
-    exports.name = 'Anonymize';
+exports.name = 'Anonymize';
 
-    exports.priority = 0;
+exports.priority = 0;
 
-    exports.version = '1.13.0';
+exports.version = '1.13.0';
 
 
-    exports.begin = function begin(browser, config) {
-        discourse = browser;
-        conf = config.modules[exports.name];
-    };
+exports.begin = function begin(browser, config) {
+    discourse = browser;
+    conf = config.modules[exports.name];
+};
 
-    exports.onNotify = function (type, notification, post, callback) {
-        if ((!conf.enabled || !post) ||
-            (!post.cleaned || type !== 'private_message')) {
-            return callback();
-        }
-        var match = rQuote.xexec(post.raw);
-        if (!match) {
-            return callback();
-        }
-        var anon = 'Anonymizied Reply Sent. Thank you for using Anonymizer, ' +
-            'a SockIndustries application.';
-        discourse.log('Posting anonymously to ' + match.topic_id);
-        discourse.createPost(match.topic_id, match.post_number, post.raw,
-            function () {
-                setTimeout(function () {
-                    discourse.createPost(notification.topic_id,
-                        notification.post_number, anon, function () {
+exports.onNotify = function (type, notification, post, callback) {
+    if ((!conf.enabled || !post) ||
+        (!post.cleaned || type !== 'private_message')) {
+        return callback();
+    }
+    var match = rQuote.xexec(post.raw);
+    if (!match) {
+        return callback();
+    }
+    var anon = 'Anonymizied Reply Sent. Thank you for using Anonymizer, ' +
+        'a SockIndustries application.';
+    discourse.log('Posting anonymously to ' + match.topic_id);
+    discourse.createPost(match.topic_id, match.post_number, post.raw,
+        function () {
+            setTimeout(function () {
+                discourse.createPost(notification.topic_id,
+                    notification.post_number, anon, function () {
 
-                        });
-                }, 500);
-                callback(true);
-            });
-    };
-
-}());
+                    });
+            }, 500);
+            callback(true);
+        });
+};
