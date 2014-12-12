cf427257c06ca4093ec81d97aed9fe0883f4479f
Accalia Elementia <accalia.de.elementia@gmail.com>
2014-12-12 08:33:49 -0500

    add backup module

---
+++ admin_modules/backup.js
@@ -0,0 +1,349 @@
+'use strict';
+
+var async = require('async'),
+    mkdirp = require('mkdirp'),
+    fs = require('fs'),
+    spawn = require('child_process').spawn;
+var discourse, config, log = [],
+    oncomplete = [];
+exports.name = 'Backups';
+exports.version = '0.0.1';
+exports.description = 'Backup Manager';
+exports.configuration = {
+    'auto_download': true,
+    'download_dir': 'backups',
+    'remove_old_backups': true,
+    'auto_restore': true,
+    'auto_backup': true,
+    'auto_backup_time': '05:00',
+    'auto_size_limit': 20971520,
+    'notify_users': 'accalia'
+};
+
+exports.begin = function begin(browser, configuration) {
+    discourse = browser;
+    config = configuration;
+    if (config.auto_backup) {
+        async.forever(function (next) {
+            autoBackup(next);
+        });
+    }
+};
+
+
+function autoBackup(callback) {
+    var hour = parseInt(/(\d+)/.exec(config.auto_backup_time)[1], 10),
+        minute = parseInt(/:(\d+)/.exec(config.auto_backup_time)[1], 10),
+        now = new Date(),
+        utc = new Date();
+    utc.setUTCHours(hour);
+    utc.setUTCMinutes(minute);
+    utc.setUTCSeconds(0);
+    utc.setMilliseconds(0);
+    now = now.getTime();
+    utc = utc.getTime();
+    if (now > utc) {
+        // add a day if scheduling after 23:40 UTC
+        utc += 24 * 60 * 60 * 1000;
+    }
+    setTimeout(function () {
+        discourse.log('Starting Scheduled Backup.');
+        startBackup(function (err) {
+            if (err) {
+                discourse.warn('Backup Failed');
+            } else {
+                discourse.log('Backup Succeeded');
+            }
+            callback();
+        });
+    }, utc - now);
+}
+
+exports.backup = function backup(args, callback) {
+    var arg = (args || []).shift() || '';
+    arg = ''.toLowerCase();
+    switch (arg) {
+    case '':
+    case 'all':
+    case 'start':
+    case 'download':
+    case 'load':
+        async.series([
+            function (flow) {
+                if (arg === 'all' || arg === 'start' || arg === '') {
+                    return startBackup(flow);
+                }
+                flow(null, []);
+            },
+            function (flow) {
+                if (arg === 'all' || arg === 'download') {
+                    return downloadLatestBackup(false, flow);
+                }
+                flow(null, []);
+            },
+            function (flow) {
+                if (arg === 'all' || arg === 'load') {
+                    return restoreBackup(flow);
+                }
+                flow(null, []);
+            }
+        ], function (err, results) {
+            var logs = err ? 'ERROR:\n' : 'SUCCESS:\n';
+            results.forEach(function (i) {
+                logs += i.join('\n');
+            });
+            callback(null, logs);
+        });
+        break;
+    default:
+        callback(null, 'Usage: !admin download [all|start|download|load]');
+        break;
+    }
+};
+exports.backup.command = 'backup';
+exports.backup.description = 'Do backup things';
+exports.backup.trustLevel = 8;
+exports.backup.muteable = false;
+exports.backup.prefix = false;
+
+function startBackup(callback) {
+    var abort = false;
+    oncomplete.push(function (err, log, after) {
+        if (abort) {
+            return;
+        }
+        callback(err, log);
+        after();
+    });
+    discourse.postForm('admin/backups', {
+        'with_uploads': false
+    }, function (err) {
+        if (err) {
+            abort = true;
+            callback(err, 'Backup failed to start');
+        }
+    });
+}
+
+function exec(command, args, pipeTo, callback) {
+    function split(data) {
+        return ('' + data).split('\n').filter(function (i) {
+            return !!i;
+        });
+    }
+    if (!callback) {
+        callback = pipeTo;
+        pipeTo = null;
+    }
+    var stdout = [],
+        stderr = [],
+        process;
+    process = spawn(command, args);
+    process.stderr.on('data', function (data) {
+        stderr = stderr.concat(split(data));
+    });
+    process.stdout.on('data', function (data) {
+        if (pipeTo) {
+            pipeTo.stdin.write(data);
+        }
+        stdout = stdout.concat(split(data));
+    });
+    process.on('close', function (code) {
+        if (pipeTo) {
+            pipeTo.stdin.end();
+        }
+        callback(code, stdout, stderr);
+    });
+    return process;
+}
+
+function afterBackup() {
+    var messages = log.slice();
+    async.series([
+        function (flow) {
+            if (!config.auto_download) {
+                return flow(true);
+            }
+            downloadLatestBackup(true, function (err, output) {
+                messages = messages.concat(output);
+                flow(err);
+            });
+        },
+        function (flow) {
+            if (!config.auto_restore) {
+                flow(true);
+            }
+            restoreBackup(function (err, output) {
+                messages = messages.concat(output);
+                flow(err);
+            });
+        }
+    ], function (err) {
+        console.log(err);
+        mailLog(messages, function () {});
+    });
+}
+
+function downloadLatestBackup(isauto, callback) {
+    var messages = [];
+    async.series([
+        function (flow) {
+            mkdirp(config.download_dir, function (err) {
+                var prefix = 'DOWNLOAD: Ensuring Downloads Directory Exists: ';
+                if (err) {
+                    messages.push(prefix + 'Failed');
+                } else {
+                    messages.push(prefix + 'Success');
+                }
+                flow(err);
+            });
+        },
+        function (flow) {
+            if (!config.remove_old_backups) {
+                return flow();
+            }
+            messages.push('DOWNLOAD: Removing Old Backups');
+            fs.readdir(config.download_dir, function (err, files) {
+                if (err) {
+                    messages.push('DOWNLOAD: List Directory Failed');
+                    return flow(err);
+                }
+                async.each(files.filter(function (i) {
+                    return /tar[.]gz$/.test(i);
+                }).map(function (i) {
+                    return config.download_dir + '/' + i;
+                }), fs.unlink, function (err) {
+                    if (err) {
+                        messages.push('DOWNLOAD: Removing Old Backups Failed');
+                    }
+                    flow(err);
+                });
+            });
+        },
+        function (flow) {
+            discourse.getForm('admin/backups.json', function (err, b, data) {
+                if (err) {
+                    messages.push('DOWNLOAD: Error Loading Backups');
+                    return flow(err);
+                }
+                if (isauto && data[0].size > config.auto_size_limit) {
+                    messages.push('DOWNLOAD: Aborting Download As Over Limit');
+                    flow(true);
+                }
+                var src = 'http:' + data[0].link;
+                var dest = config.download_dir + '/' + data[0].filename;
+                discourse.saveFile(src, dest, function (err) {
+                    if (err) {
+                        messages.push('DOWNLOAD: Error Downloading Backup');
+                    } else {
+                        messages.push('DOWNLOAD: Downloaded Backup');
+                    }
+                    flow(err);
+                });
+            });
+        }
+    ], function (err) {
+        callback(err, messages);
+    });
+}
+
+function restoreBackup(callback) {
+    var src, messages = [];
+    async.series([
+        function (flow) {
+            fs.readdir(config.download_dir, function (err, files) {
+                if (err) {
+                    messages.push('RESTORE: No Backup Found');
+                    return flow(err);
+                }
+                var file = files.filter(function (i) {
+                    return /tar[.]gz$/.test(i);
+                });
+                file.sort();
+                file = file.pop();
+                if (!file) {
+                    return flow(true);
+                }
+                messages.push('RESTORE: Restoring ' + file);
+                src = config.download_dir + '/' + file;
+                flow();
+            });
+        },
+        function (flow) {
+            exec('psql', [
+                '-d', 'discourse', '-c',
+                'DROP SCHEMA backup CASCADE'
+            ], function () {
+                messages.push('RESTORE: Dropped Old `restore` Schema');
+                flow();
+            });
+        },
+        function (flow) {
+            var psql = exec('psql', ['-d', 'discourse'], function (err) {
+                if (err) {
+                    messages.push('RESTORE: Load to `restore` Schema Failed');
+                    return flow(err);
+                }
+                messages.push('RESTORE: Load to `restore` Schema Done');
+                flow();
+            });
+            exec('tar', ['-xOzf', src, 'dump.sql'], psql, function () {});
+        },
+        function (flow) {
+            exec('psql', [
+                '-d', 'discourse', '-c',
+                'ALTER SCHEMA public RENAME TO backup'
+            ], function (err, out, errors) {
+                if (err) {
+                    messages.push('RESTORE: Rename Current Schema Failed');
+                    messages = messages.concat(errors);
+                    return flow(err);
+                }
+                messages.push('RESTORE: Rename Current Schema Succeded');
+                flow();
+            });
+        },
+        function (flow) {
+            exec('psql', [
+                '-d', 'discourse', '-c',
+                'ALTER SCHEMA restore RENAME TO public'
+            ], function (err, out, errors) {
+                if (err) {
+                    messages.push('RESTORE: Restore Database Failed');
+                    messages = messages.concat(errors);
+                    return flow(err);
+                }
+                messages.push('RESTORE: Restore Database Succeded');
+                flow();
+            });
+        }
+    ], function (err) {
+        callback(err, messages);
+    });
+}
+
+function mailLog(lines, callback) {
+    var msg = 'Backup completed: \n\n```text\n' + lines.join('\n') + '\n```\n';
+    discourse.createPrivateMessage(config.notify_users, 'Backup Completed', msg,
+        callback);
+}
+
+exports.registerListeners = function registerListeners(callback) {
+    callback(null, ['/admin/backups/logs']);
+};
+
+exports.onMessage = function onMessage(message, post, callback) {
+    if (message.data && message.data.operation === 'backup') {
+        if (message.data.message === '[STARTED]') {
+            log = [];
+        }
+        if (!/^pg_dump/.test(message.data.message)) {
+            log.push(message.data.message);
+        }
+        if (message.data.message === '[SUCCESS]' ||
+            message.data.message === '[FAILED]') {
+            afterBackup();
+        }
+    }
+    callback();
+};
