6528bc3e10b2043b15c3b77a8fdaf0ffbef6d717
riking <rikingcoding@gmail.com>
2015-02-10 00:39:38 -0800

    Justify output

--- sock_modules/trust.js
+++ sock_modules/trust.js
@@ -61,26 +61,26 @@ function getReportBase(user) {
         base += '\n-- no trust level 3 requirements info--\n';
     } else {
         base += '\nIn the last %time_period%:\n' +
-            'Visits: %days_visited%/%min_days_visited%\n' +
-            'Topics Replied To: %num_topics_replied_to%/' +
+            'Visits:              %days_visited%/%min_days_visited%\n' +
+            'Topics Replied To:   %num_topics_replied_to%/' +
             '%min_topics_replied_to%\n' +
-            'Topics Viewed: %topics_viewed%/%min_topics_viewed%\n' +
-            'Topics Viewed (all time): %topics_viewed_all_time%/' +
+            'Topics Viewed:       %topics_viewed%/%min_topics_viewed%\n' +
+            'Total Topics Viewed: %topics_viewed_all_time%/' +
             '%min_topics_viewed_all_time%\n' +
-            'Posts Read: %posts_read%/%min_posts_read%\n' +
-            'Posts Read (all time): %posts_read_all_time%/' +
+            'Posts Read:          %posts_read%/%min_posts_read%\n' +
+            'Total Posts Read:    %posts_read_all_time%/' +
             '%min_posts_read_all_time%\n' +
-            'Flagged: %num_flagged_posts%/%max_flagged_posts% (max)\n' +
-            'Flagged By: %num_flagged_by_users%/' +
+            'Flagged Posts:       %num_flagged_posts%/%max_flagged_posts% (max)\n' +
+            'Flagged By:          %num_flagged_by_users%/' +
             '%max_flagged_by_users% (max)\n' +
-            'Likes Given: %num_likes_given%/%min_likes_given%\n' +
-            'Likes Received: %num_likes_received%/%min_likes_received%\n' +
-            'Liked on Days: %num_likes_received_days%/' +
+            'Likes Given:         %num_likes_given%/%min_likes_given%\n' +
+            'Likes Received:      %num_likes_received%/%min_likes_received%\n' +
+            'Liked on Days:       %num_likes_received_days%/' +
             '%min_likes_received_days%\n' +
-            'Liked by users: %num_likes_received_users%/' +
+            'Liked by users:      %num_likes_received_users%/' +
             '%min_likes_received_users%\n' +
             '\n' +
-            'Status: %analysis%\n';
+            'Status:  %analysis%\n';
     }
     base += '```\n';
     return base;
