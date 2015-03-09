43b6b61f8b6fe80c71fae5c35f81d8a4c08ab6b1
Accalia Elementia <accalia.de.elementia@gmail.com>
2015-03-09 11:12:15 -0400

    make lack of tingo non fatal

--- database.js
+++ database.js
@@ -1,17 +1,26 @@
 'use strict';
-var fs = require('fs'),
-    tingo = require('tingodb');
+var fs = require('fs');
 var user,
-    Database = tingo().Db;
+    tingo, Database;
 
-exports.begin = function begin(unused, config) {
+exports.begin = function begin(discourse, config) {
+    try {
+        tingo = require('tingodb');
+        Database = tingo().Db;
+    } catch (e) {
+        discourse.warn('Error loading tingo database driver: ' + e);
+    }
     user = config.username;
 };
 
 exports.getDatabase = function getDatabase(collection, callback) {
-    var path = 'databases/' + user + '/', db;
-    fs.mkdir(path, function(err){
-        if (err && err.code !== 'EEXIST'){
+    if (!Database) {
+        callback('No Database Driver', null);
+    }
+    var path = 'databases/' + user + '/',
+        db;
+    fs.mkdir(path, function (err) {
+        if (err && err.code !== 'EEXIST') {
             return callback(err, null);
         }
         db = new Database(path, {}).collection(collection);
