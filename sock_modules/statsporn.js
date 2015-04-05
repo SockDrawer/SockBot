1d3f669a970acb3ca357dba3c2c0f372eacaeef3
Accalia <Accalia.de.Elementia@gmail.com>
2015-04-05 11:21:03 +0000

    fix NPE for queries without defauls

--- sock_modules/statsporn.js
+++ sock_modules/statsporn.js
@@ -258,14 +258,14 @@ function listQueries(notification, callback) {
     if (!queries) {
         discourse.warn('Queries not loaded');
         return discourse.createPost(notification.topic_id,
-            notification.post_number, 'No queries available;'
-            + 'someone probably @&shy;accalia\'d the YAML :blush:',
+            notification.post_number, 'No queries available;' + 'someone probably @&shy;accalia\'d the YAML :blush:',
             function () {
-            callback(true);
-        });
+                callback(true);
+            });
     }
     var res = queries.map(function (q) {
         var args;
+        q.config.defaults = q.config.defaults || [];
         for (var i = 0; i <= 10; i += 1) {
             if (q.config.defaults[i]) {
                 args = '\'' + q.config.defaults[i].join('\' \'') + '\'';
