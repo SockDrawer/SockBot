812b95f0fb0134dc9c0602c270a185db29411555
yamikuronue <yamikuronue@gmail.com>
2015-04-21 14:08:51 +0000

    Adding mkdocs for static docs pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,24 +5,32 @@ module.exports = function(grunt) {
 	gitclone: {
 		docs: {
 			options: {
-				directory: "docs",
+				directory: "site",
 				repository: "https://github.com/SockDrawer/SockBot.git",
 				branch: "gh-pages"
 			}
+		}
+	},
+	mkdocs: {
+		dist: {
+		  src: '.',
+		  options: {
+			clean: true
+		  }
 		}
 	},
 	jsdoc : {
         dist : {
             src: ['*.js', 'sock-modules/**/*.js'],
             options: {
-                destination: 'docs'
+                destination: 'site/docs'
             }
         }
     },
 	'gh-pages': {
 		options: {
 			// The default commit message for the gh-pages branch
-			base: 'docs',
+			base: 'site',
 			message: 'push documentation automatically',
 			repo: 'https://' + process.env.GH_TOKEN + '@github.com/SockDrawer/SockBot'
 		},
@@ -31,12 +39,13 @@ module.exports = function(grunt) {
   });
 
   // Load the plugins
+    grunt.loadNpmTasks('grunt-mkdocs');
   grunt.loadNpmTasks('grunt-git');
   grunt.loadNpmTasks('grunt-jsdoc');
   grunt.loadNpmTasks('grunt-gh-pages');
 
   // Default task(s).
-  grunt.registerTask('generate-docs', ['jsdoc', 'gh-pages']);
-  grunt.registerTask('default', ['jsdoc', 'gh-pages']);
+  grunt.registerTask('generate-docs', ['mkdocs', 'jsdoc', 'gh-pages']);
+  grunt.registerTask('default', ['mkdocs', 'jsdoc', 'gh-pages']);
 
 };
