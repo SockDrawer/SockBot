68132ba5f1b53df981b536f5b21dc7f1e2bf3204
RaceProUK <raceprouk@hotmail.co.uk>
2015-04-22 11:34:42 +0100

    Fixes #92, #95, #96 - Documentation
    I hope I've done this right…

--- sock_modules/mathematriculator.js
+++ sock_modules/mathematriculator.js
@@ -1,15 +1,35 @@
+/**
+ * Mathematriculator module. Responsible for automatically performing mathematical operations and unit conversions
+ * @module markov
+ */
 'use strict';
 var math = require('mathjs'),
     async = require('async');
 var errors;
 
+/** Name of the module */
 exports.name = 'Math';
+
+/** Module version */
 exports.version = '0.2.0';
+
+/** Description of the module */
 exports.description = 'Do mathematics!';
+
+/** 
+ * Configuration properties.
+ * @property enabled - Whether to use Markov or not. Defaults to false.
+ */
 exports.configuration = {
     enabled: false
 };
 
+/**
+ * Respond to commands.
+ * @param {string} command - The command issued
+ * @param {array} args - A list of arguments for the command
+ * @param {function} callback - The callback to use once the action is complete
+ */
 exports.onCommand = function onCommand(_, command, args, __, callback) {
     if (command.toLowerCase() !== 'calc'){
         args.unshift(command);
@@ -17,6 +37,10 @@ exports.onCommand = function onCommand(_, command, args, __, callback) {
     calc(args, callback);
 };
 
+/**
+ * Bootstrap the module.
+ * @param {object} config - The SockBot config object
+ */
 exports.begin = function begin(_, config) {
     errors = config.errors;
     math.config({
@@ -25,6 +49,11 @@ exports.begin = function begin(_, config) {
     });
 };
 
+/**
+ * Bootstrap the module.
+ * @param {array} args - A list of arguments to combine into an evaluateable expression
+ * @param {function} callback - The callback to use once the action is complete
+ */
 function calc(args, callback) {
     async.series([function (cb) {
             var realExpression = args.join(' ');
