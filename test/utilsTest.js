5d54b59f1d257680e17c34dd46b9aeae5cbd775e
Accalia de Elementia <accalia.de.elementia@gmail.com>
2015-06-19 13:45:34 +0000

    tests for utils.js

--- test/utilsTest.js
+++ test/utilsTest.js
@@ -40,4 +40,58 @@ describe('utils', () => {
             uuid.should.match(/^[\da-f]{8}-[\da-f]{4}-4[\da-f]{3}-[\da-f]{4}-[\da-f]{12}$/);
         });
     });
+    describe('addTimestamp()', () => {
+        it('should add correct format time format', () => {
+            let message = utils.addTimestamp('message');
+            message.should.match(/^\[\d{2}:\d{2}:\d{2}\] message$/);
+        });
+    });
+    describe('log()', () => {
+        it('should call console.log()', () => {
+            /* eslint-disable no-console */
+            const log = console.log;
+            try {
+                console.log = sinon.spy();
+                utils.log('message');
+                console.log.called.should.be.true;
+                console.log.lastCall.args.should.have.length(1);
+                console.log.lastCall.args[0].should.match(/^\[\d{2}:\d{2}:\d{2}\] message$/);
+            } finally {
+                console.log = log;
+            }
+            /* eslint-enable no-console */
+        });
+    });
+    describe('warn()', () => {
+        it('should call console.warn()', () => {
+            /* eslint-disable no-console */
+            const warn = console.warn;
+            try {
+                console.warn = sinon.spy();
+                utils.warn('message');
+                console.warn.called.should.be.true;
+                console.warn.lastCall.args.should.have.length(1);
+                console.warn.lastCall.args[0].should.match(/^\[\d{2}:\d{2}:\d{2}\] message$/);
+            } finally {
+                console.warn = warn;
+            }
+            /* eslint-enable no-console */
+        });
+    });
+    describe('error()', () => {
+        it('should call console.error()', () => {
+            /* eslint-disable no-console */
+            const error = console.error;
+            try {
+                console.error = sinon.spy();
+                utils.error('message');
+                console.error.called.should.be.true;
+                console.error.lastCall.args.should.have.length(1);
+                console.error.lastCall.args[0].should.match(/^\[\d{2}:\d{2}:\d{2}\] message$/);
+            } finally {
+                console.error = error;
+            }
+            /* eslint-enable no-console */
+        });
+    });
 });
