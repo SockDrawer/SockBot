ff6849f1890a2a84f0f6d4cbd4e6b913acb768ed
Accalia de Elementia <accalia.de.elementia@gmail.com>
2015-06-22 11:23:23 +0000

    add async queue to browser for throttled wrb requests

--- browser.js
+++ browser.js
@@ -2,23 +2,64 @@
 /**
  * Webbrowser abstraction for communicating with discourse
  * @module browser
- * @licenst MIT
+ * @license MIT
  */
 
-let request = require('request');
-const utils = require('./utils');
+const request = require('request'),
+    async = require('async');
+
 const defaults = {
-    rejectUnauthorized: false,
-    jar: request.jar(),
-    headers: {
-        'X-Requested-With': 'XMLHttpRequest',
-        'User-Agent': 'SockBot 2.0.x Angelic Ariel'
-    }
-};
-request = request.defaults(defaults);
+        rejectUnauthorized: false,
+        jar: request.jar(),
+        headers: {
+            'X-Requested-With': 'XMLHttpRequest',
+            'User-Agent': 'SockBot 2.0.x Angelic Ariel'
+        }
+    },
+    internals = {
+        request: request.defaults(defaults),
+        queue: async.queue(queueWorker, 1),
+        queueWorker: queueWorker,
+        defaults: defaults
+    };
+
+/**
+ * Process browser tasks with rate limiting
+ *
+ * @param {object} task Task configuration
+ * @param {string} [task.method=GET] HTTP method to request
+ * @param {string} task.url Site relative URL to request
+ * @param {object} [task.form] HTTP form to use in HTTP request
+ * @param {discourse~requestComplete} [task.callback] Callback toprovide request results to
+ * @param {Number} [task.delay=0] Seconds to delay callback after request for additional rate limiting
+ * @param {Function} callback Queue task complete callback
+ */
+function queueWorker(task, callback) {
+    internals.request({
+        url: task.url,
+        method: task.method || 'GET',
+        form: task.form
+    }, (e, _, body) => {
+        try {
+            body = JSON.parse(body);
+        } catch (ignore) {} //eslint-disable-line no-empty
+        if (task.callback && typeof task.callback === 'function') {
+            setTimeout(() => task.callback(e, body), task.delay || 0);
+        }
+        setTimeout(callback, 5000);
+    });
+}
+
+/**
+ * Browser Request Callback
+ * @param {Exception} [err=null] Error encountered processing request
+ * @param {Object} body JSON parsed response body. If invalid JSON will be `undefined`
+ */
+function requestComplete(err, body) {} //eslint-disable-line handle-callback-err, no-unused-vars
 
 /* istanbul ignore else */
 if (typeof GLOBAL.describe === 'function') {
     //test is running
-    exports.defaults = defaults;
+    internals.requestComplete = requestComplete;
+    exports.internals = internals;
 }
--- test/browserTest.js
+++ test/browserTest.js
@@ -1,5 +1,5 @@
 'use strict';
-/*globals describe, it*/
+/*globals describe, it, before, after*/
 /*eslint no-unused-expressions:0 */
 
 const chai = require('chai'),
@@ -13,7 +13,7 @@ const browser = require('../browser');
 describe('browser', () => {
     describe('exports', () => {
         const fns = [],
-            objs = [],
+            objs = ['internals'],
             vals = [];
         describe('should export expected functions:', () => {
             fns.forEach((fn) => {
@@ -30,8 +30,188 @@ describe('browser', () => {
                 it(val, () => browser.should.have.key(val));
             });
         });
-        //it('should export only expected keys', () => {
-        //    browser.should.have.all.keys(fns.concat(objs, vals));
-        //});
+        it('should export only expected keys', () => {
+            browser.should.have.all.keys(fns.concat(objs, vals));
+        });
+    });
+    describe('internals', () => {
+        const fns = ['queueWorker', 'requestComplete', 'request'],
+            objs = ['defaults', 'queue'],
+            vals = [];
+        describe('should include expected functions:', () => {
+            fns.forEach((fn) => {
+                it(fn + '()', () => expect(browser.internals[fn]).to.be.a('function'));
+            });
+        });
+        describe('should include expected objects', () => {
+            objs.forEach((obj) => {
+                it(obj, () => expect(browser.internals[obj]).to.be.a('object'));
+            });
+        });
+        describe('should include expected values', () => {
+            vals.forEach((val) => {
+                it(val, () => browser.internals.should.have.key(val));
+            });
+        });
+        it('should include only expected keys', () => {
+            browser.internals.should.have.all.keys(fns.concat(objs, vals));
+        });
+    });
+    describe('queueWorker()', () => {
+        const queueWorker = browser.internals.queueWorker,
+            request = browser.internals.request;
+        let clock;
+        before(() => {
+            clock = sinon.useFakeTimers();
+        });
+        it('should delay completion by five seconds to throttle requests', (done) => {
+            browser.internals.request = (_, cb) => cb(null, null, '"foobar"');
+            const spy = sinon.spy();
+            queueWorker({}, spy);
+            spy.called.should.be.false;
+            clock.tick(5000);
+            spy.called.should.be.true;
+            done();
+        });
+        it('should select default callback delay when omitted', (done) => {
+            browser.internals.request = (_, cb) => cb();
+            const queueSpy = sinon.spy(),
+                callbackSpy = sinon.spy();
+            queueWorker({
+                callback: callbackSpy
+            }, queueSpy);
+            clock.tick(0);
+            queueSpy.called.should.be.false;
+            callbackSpy.called.should.be.true;
+            clock.tick(5000);
+            queueSpy.called.should.be.true;
+            done();
+        });
+        it('should select callback delay from task', (done) => {
+            browser.internals.request = (_, cb) => cb();
+            const queueSpy = sinon.spy(),
+                callbackSpy = sinon.spy();
+            queueWorker({
+                callback: callbackSpy,
+                delay: 1000
+            }, queueSpy);
+            clock.tick(0);
+            queueSpy.called.should.be.false;
+            callbackSpy.called.should.be.false;
+            clock.tick(1000);
+            callbackSpy.called.should.be.true;
+            clock.tick(5000);
+            queueSpy.called.should.be.true;
+            done();
+        });
+        it('should accept invalid JSON', (done) => {
+            browser.internals.request = (_, cb) => cb(null, null, '"foobar');
+            const queueSpy = sinon.spy(),
+                callbackSpy = sinon.spy();
+            queueWorker({
+                callback: callbackSpy
+            }, queueSpy);
+            clock.tick(0);
+            queueSpy.called.should.be.false;
+            callbackSpy.called.should.be.true;
+            callbackSpy.lastCall.args.should.have.length(2);
+            expect(callbackSpy.lastCall.args[0]).to.be.null;
+            callbackSpy.lastCall.args[1].should.equal('"foobar');
+            clock.tick(5000);
+            queueSpy.called.should.be.true;
+            done();
+        });
+        it('should pass errors to task.callback', (done) => {
+            browser.internals.request = (_, cb) => cb('ERROR', null, null);
+            const queueSpy = sinon.spy(),
+                callbackSpy = sinon.spy();
+            queueWorker({
+                callback: callbackSpy
+            }, queueSpy);
+            clock.tick(0);
+            callbackSpy.called.should.be.true;
+            callbackSpy.lastCall.args.should.have.length(2);
+            callbackSpy.lastCall.args[0].should.equal('ERROR');
+            expect(callbackSpy.lastCall.args[1]).to.be.null;
+            clock.tick(5000);
+            queueSpy.called.should.be.true;
+            done();
+        });
+        it('should use default method when omitted', (done) => {
+            let req;
+            browser.internals.request = (opts, cb) => {
+                req = opts;
+                cb();
+            };
+            const queueSpy = sinon.spy();
+            queueWorker({}, queueSpy);
+            clock.tick(5000);
+            queueSpy.called.should.be.true;
+            req.method.should.equal('GET');
+            done();
+        });
+        it('should select method from task', (done) => {
+            let req;
+            browser.internals.request = (opts, cb) => {
+                req = opts;
+                cb();
+            };
+            const queueSpy = sinon.spy();
+            queueWorker({
+                method: 'FRED'
+            }, queueSpy);
+            clock.tick(5000);
+            queueSpy.called.should.be.true;
+            req.method.should.equal('FRED');
+            done();
+        });
+        it('should select URL from task', (done) => {
+            let req;
+            browser.internals.request = (opts, cb) => {
+                req = opts;
+                cb();
+            };
+            const queueSpy = sinon.spy();
+            queueWorker({
+                url: 'http://example.com'
+            }, queueSpy);
+            clock.tick(5000);
+            queueSpy.called.should.be.true;
+            req.url.should.equal('http://example.com');
+            done();
+        });
+
+        it('should select form from task', (done) => {
+            const form = {
+                a: 1,
+                b: 2,
+                c: 4,
+                d: 3
+            };
+            let req;
+            browser.internals.request = (opts, cb) => {
+                req = opts;
+                cb();
+            };
+            const queueSpy = sinon.spy();
+            queueWorker({
+                form: form
+            }, queueSpy);
+            clock.tick(5000);
+            queueSpy.called.should.be.true;
+            req.form.should.deep.equal(form);
+            done();
+        });
+        after(() => {
+            clock.restore();
+            browser.internals.request = request;
+        });
+    });
+    describe('documentation stubs', () => {
+        const internals = browser.internals,
+            stubs = ['requestComplete'];
+        stubs.forEach((stub) => {
+            it(stub + '() should be a stub function', () => internals[stub]());
+        });
     });
 });
