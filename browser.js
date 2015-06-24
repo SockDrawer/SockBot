0015b6076ca29113d55c7008da38d3a69be9a15d
Accalia de Elementia <accalia.de.elementia@gmail.com>
2015-06-24 02:09:37 +0000

    add mroe functionality and matching tests to browser.js

--- browser.js
+++ browser.js
@@ -8,6 +8,46 @@
 const request = require('request'),
     async = require('async');
 
+const utils = require('./utils');
+
+/**
+ * BBcode Quote Tag Parsing/Stripping Regexp
+ *
+ * Contributed by Flabdablet
+ *
+ * A BBCode quote tag is like
+ *     [quote]
+ * or
+ *     [quote=anything-that-isn't-a-closing-bracket]
+ *
+ * and the regex uses (=[^\]]*)? to deal with the difference. The section that matches the closing tag \[\/quote] is
+ * straightforward.
+ *
+ * The regex is supposed to match only the innermost of any nested BBCode quotes, so between the matching parts for
+ * opening and closing tags, it matches only text that doesn't contain the sequence [quote= or [quote].
+ *
+ * This is done by a *? lazy-repeat match on
+ *
+ * (        a group containing
+ *
+ * [^\[]    anything that isn't a [
+ * |        or
+ *     (        a group containing
+ *     \[       an opening square bracket followed by
+ *     [^q]     anything that isn't a q
+ *     |        or
+ *         (        a group containing
+ *         q        a q followed by
+ *         [^u]     anything that isn't a u
+ * ...
+ *
+ * The net effect is to match on text made of chunks that can be [quote as long as that isn't part of [quote= or
+ * [quote], or [quot that isn't part of [quote, or [quo that isn't part of [quot, and so on all the way back to single
+ * characters that aren't [.
+ *
+ * And just for icing on the line noise cake, all groups are specified as non-capturing (?: for speed.
+ */
+const rQuote = /\[quote(?:=[^\]]*)?](?:[^\[]|\[(?:[^q]|q(?:[^u]|u(?:[^o]|o(?:[^t]|t(?:[^e]|e[^=\]]))))))*?\[\/quote]/ig;
 const defaults = {
         rejectUnauthorized: false,
         jar: request.jar(),
@@ -20,7 +60,10 @@ const defaults = {
         request: request.defaults(defaults),
         queue: async.queue(queueWorker, 1),
         queueWorker: queueWorker,
-        defaults: defaults
+        defaults: defaults,
+        rQuote: rQuote,
+        rCode: /^```.*\n(?:.*\n)*?```\n/gm,
+        rNewLine: /\r\n/g
     };
 
 /**
@@ -30,7 +73,7 @@ const defaults = {
  * @param {string} [task.method=GET] HTTP method to request
  * @param {string} task.url Site relative URL to request
  * @param {object} [task.form] HTTP form to use in HTTP request
- * @param {discourse~requestComplete} [task.callback] Callback toprovide request results to
+ * @param {browser~requestComplete} [task.callback] Callback toprovide request results to
  * @param {Number} [task.delay=0] Seconds to delay callback after request for additional rate limiting
  * @param {Function} callback Queue task complete callback
  */
@@ -51,15 +94,85 @@ function queueWorker(task, callback) {
 }
 
 /**
+ * Strip [quote] tags from input
+ *
+ * @param {string} input Input string to strip
+ * @returns {string} Input after stripping [quote] tags
+ */
+function stripQuotes(input) {
+    /*
+    let result;
+    while ((result = input.replace(internals.rQuote, '')) !== input) {
+        input = result;
+    }
+    return result;
+    */
+    // Generate a string with no chance of pre-existing in the text.
+    const placeholder = utils.uuid();
+
+    // Repeatedly remove regex matches until nothing matches it
+    // any more, thereby stripping out BBCode quotes from the most
+    // deeply nested outward. Dump a placeholder into the text
+    // to mark each edit.
+    let edited;
+    while ((edited = input.replace(internals.rQuote, placeholder)) !== input) {
+        input = edited;
+    }
+
+    // Now strip out all the placeholders along with any unbalanced
+    // quote tags that lead them.
+    const garbage = new RegExp(
+        '(?:\\[quote(?:=[^\\]]*)?][^]*?)?' + placeholder,
+        'ig'
+    );
+    return edited.replace(garbage, '');
+}
+internals.stripQuotes = stripQuotes;
+
+/**
+ * Strip GFM fenced code blocks from input
+ *
+ * @param {string} input Input string to strip
+ * @returns {string} Input after stripping GFM code blocks tags
+ */
+function stripCode(input) {
+    let result;
+    input = input + '\n';
+    while ((result = input.replace(internals.rCode, '')) !== input) {
+        input = result;
+    }
+    return result.replace(/\n$/m, '');
+}
+internals.stripCode = stripCode;
+
+/**
+ * Clean post raw
+ *
+ * @param {discourse~Post} post Post to clean
+ * @param {string} post.raw Raw text of the post to clean
+ * @returns {discourse~CleanedPost} input post with cleaned raw
+ */
+function cleanPost(post) {
+    let text = post.raw || '';
+    text = text.replace(internals.rNewLine, '\n');
+    text = stripCode(text);
+    text = stripQuotes(text);
+    post.cleaned = text;
+    return post;
+}
+internals.cleanPost = cleanPost;
+
+/**
  * Browser Request Callback
+ *
  * @param {Exception} [err=null] Error encountered processing request
  * @param {Object} body JSON parsed response body. If invalid JSON will be `undefined`
  */
 function requestComplete(err, body) {} //eslint-disable-line handle-callback-err, no-unused-vars
+internals.requestComplete = requestComplete;
 
 /* istanbul ignore else */
 if (typeof GLOBAL.describe === 'function') {
     //test is running
-    internals.requestComplete = requestComplete;
     exports.internals = internals;
 }
--- test/browserTest.js
+++ test/browserTest.js
@@ -35,8 +35,8 @@ describe('browser', () => {
         });
     });
     describe('internals', () => {
-        const fns = ['queueWorker', 'requestComplete', 'request'],
-            objs = ['defaults', 'queue'],
+        const fns = ['queueWorker', 'requestComplete', 'request', 'stripQuotes', 'stripCode', 'cleanPost'],
+            objs = ['defaults', 'queue', 'rQuote', 'rCode', 'rNewLine'],
             vals = [];
         describe('should include expected functions:', () => {
             fns.forEach((fn) => {
@@ -45,7 +45,7 @@ describe('browser', () => {
         });
         describe('should include expected objects', () => {
             objs.forEach((obj) => {
-                it(obj, () => expect(browser.internals[obj]).to.be.a('object'));
+                it(obj, () => (typeof browser.internals[obj]).should.equal('object'));
             });
         });
         describe('should include expected values', () => {
@@ -213,5 +213,148 @@ describe('browser', () => {
         stubs.forEach((stub) => {
             it(stub + '() should be a stub function', () => internals[stub]());
         });
+    });
+    describe('internals.rQuote', () => {
+        const rQuote = browser.internals.rQuote;
+        describe('should match known passing tests', () => {
+            ['[quote="accalia, post:108, topic:49440, full:true"][/quote]',
+                '[quote="accalia, post:108, topic:49440, full:true"]\nthis is a quote[/quote]',
+                '[quote="accalia, post:108, topic:49440, full:true"]\r\nthis is a quote\r\n\r\n[/quote]',
+                '[quote="accalia, post:108, topic:49440"][/quote]',
+                '[quote="accalia, post:108, topic:49440"]\nthis is a quote [/quote]',
+                '[quote="accalia, post:108, topic:49440"]this is a quote[/quote]',
+                '[quote="accalia, post:108"][/quote]', '[quote="accalia, post:108"]\nthis is a quote\n\r\n[/quote]',
+                '[quote="accalia"][/quote]', '[quote="accalia"]this is a quote\r\n\r\n[/quote]',
+                '[quote=accalia][/quote]', '[quote=accalia]this is a quote[/quote]',
+                '[quote][/quote]', '[quote]\n\nthis is a quote[/quote]'
+            ].forEach((quote) => {
+                it('should match: ' + quote, () => {
+                    rQuote.test(quote).should.be.true;
+                    quote.replace(rQuote, '').should.equal('');
+                });
+            });
+        });
+        describe('should not match known failing tests', () => {
+            ['[quote="accalia, post:108, topic:49440, full:true"]',
+                '[quote="accalia, post:108, topic:49440, full:true"this is a quote[/quote]',
+                '[quote="accalia, post:108, topic:49440, full:true"]this is a quote/quote]',
+                '[quote="accalia, post:108, topic:49440"][/quote',
+                '[quote="accalia, post:108, topic:49440"]this is a quote [quote]',
+                '[/quote]',
+                '[quoteaccalia, post:108"][/quote]'
+            ].forEach((quote) => {
+                it('should match: ' + quote, () => {
+                    rQuote.test(quote).should.be.false;
+                    quote.replace(rQuote, '').should.not.equal('');
+                });
+            });
+        });
+    });
+    describe('internals.stipQuotes', () => {
+        const stripQuotes = browser.internals.stripQuotes;
+        describe('should match known passing tests', () => {
+            [
+                ['[quote=a]i am stripped[/quote]', ''],
+                ['this is\n[quote=accalia][/quote] \r\na quote', 'this is\n \r\na quote'],
+                ['this[quote]nope[quote]no[/quote]nada[/quote] survives', 'this survives'],
+                ['i am not a [quote]', 'i am not a [quote]'],
+                ['this[quote]nope[quote]no[quote]nada[/quote] survives', 'this survives'],
+                ['[quote="accalia, post:108, topic:49440, full:true"]\nthis is a quote[/quote] inner words ' +
+                    '[quote="accalia, post:108, topic:49440, full:true"]\nthis is another quote[/quote]',
+                    ' inner words '
+                ],
+                ['before words\n[quote="accalia, post:108, topic:49440, full:true"]\nthis is a ' +
+                    'quote[/quote] inner words [quote="accalia, post:108, topic:49440, full:true"]\nthis is another ' +
+                    'quote[/quote]\nafter words',
+                    'before words\n inner words \nafter words'
+                ]
+            ].forEach((test) => {
+                const quote = test[0],
+                    result = test[1];
+                it('should clean: ' + quote, () => {
+                    stripQuotes(quote).should.equal(result);
+                });
+            });
+        });
+    });
+    describe('internals.stripCode()', () => {
+        const stripCode = browser.internals.stripCode;
+        describe('simple whole input matching tests', () => {
+            ['```\nfoo();\n```', '```javascript\nfoo();\n```', '```\n``\n```',
+                '```\n\n```', '```\n```', '``` \nfoo();\n```'
+            ].forEach((test) => {
+                it('should strip: ' + test, () => stripCode(test).should.equal(''));
+            });
+        });
+        describe('negative input matching tests', () => {
+            ['```\n```test', '```\n``` ', '```foo```',
+                '```\ntest();```', ' ```\ntest();\n```'
+            ].forEach((test) => {
+                it('should not strip: ' + test, () => stripCode(test).should.equal(test));
+            });
+        });
+        describe('test strip text', () => {
+            [
+                ['```\ncode\n```', ''],
+                ['before\n```\ncode\n```', 'before'],
+                ['before2\n```\ncode\n```\n', 'before2\n'],
+                ['```\ncode\n```\nbetween\n```\ncode\n```', 'between'],
+                ['```\ncode\n```\nbetween2\n```\ncode\n```\n', 'between2\n'],
+                ['```\ncode\n```\nafter', 'after'],
+                ['```type\ncode\n```', ''],
+                ['```\ncode\n```\nnotcode\n```\n', 'notcode\n```\n'],
+                ['before\n```\n```\nafter', 'before\nafter'],
+                ['before\n```\n```\nmiddle\n```\nafter', 'before\nmiddle\n```\nafter'],
+                ['before\n```\n1\n```\nmiddle\n```\n2\n```\nafter', 'before\nmiddle\nafter']
+            ].forEach((test => {
+                const input = test[0],
+                    output = test[1];
+                it('should strip: ' + input, () => stripCode(input).should.equal(output));
+            }));
+        });
+    });
+    describe('internals.rNewLine', () => {
+        const rNewLine = browser.internals.rNewLine;
+        it('should match windows style newline', () => rNewLine.test('\r\n').should.be.true);
+        it('should not match linux style newline', () => rNewLine.test('\r').should.be.false);
+        it('should not match mac style newline', () => rNewLine.test('\r').should.be.false);
+        it('should separated sentinal characters', () => rNewLine.test('\r \n').should.be.false);
+
+    });
+    describe('internals.cleanPost()', () => {
+        const cleanPost = browser.internals.cleanPost;
+        it('should add `cleaned` attribute to input', () => {
+            const post = {
+                raw: 'hello!'
+            };
+            post.should.not.have.any.key('cleaned');
+            cleanPost(post);
+            post.should.have.any.key('cleaned');
+        });
+        it('should accept empty post', () => cleanPost({}).cleaned.should.equal(''));
+        describe('should normalize newlines', () => {
+            [
+                ['this is normal\r\n', 'this is normal\n'],
+                ['this is normal\r\n\r\n', 'this is normal\n\n'],
+                ['this\ris normal\r\n', 'this\ris normal\n'],
+                ['this is\nnormal\r\n', 'this is\nnormal\n'],
+                ['this\r\nis normal\r\n', 'this\nis normal\n']
+            ].forEach((test) => {
+                const input = test[0],
+                    output = test[1];
+                it('should correctly noprmalize: ' + JSON.stringify(input), () => {
+                    const result = cleanPost({
+                        raw: input
+                    }).cleaned;
+                    result.should.to.equal(output);
+                });
+            });
+        });
+        it('should strip GFM code blocks', () => cleanPost({
+            raw: '```\ncode\n```\n'
+        }).cleaned.should.to.equal(''));
+        it('should strip quotes blocks', () => cleanPost({
+            raw: '[quote]quoted[/quote]\n'
+        }).cleaned.should.to.equal('\n'));
     });
 });
