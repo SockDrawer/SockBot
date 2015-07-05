c9095861cc7da51dd2d79c073ffb804a1e678de0
Accalia de Elementia <accalia.de.elementia@gmail.com>
2015-07-05 22:28:07 +0000

    set ignore on main metod for istanbul.

--- SockBot.js
+++ SockBot.js
@@ -6,7 +6,16 @@
  * @license MIT
  */
 
+/*eslint-disable no-console */
 exports.version = 'v2.0.0';
 
 
 
+const config = require('./config'),
+    browser = require('./browser');
+/* istanbul ignore if */
+if (require.main === module) {
+    config.loadConfiguration(process.argv[2], () => {
+        browser.login(() => console.log(arguments));
+    });
+}
