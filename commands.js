8cd6d301e657fa87885599b083f4cb21dc3561eb
Accalia de Elementia <accalia.de.elementia@gmail.com>
2015-07-06 12:23:51 +0000

    add docs

--- commands.js
+++ commands.js
@@ -1,7 +1,7 @@
 'use strict';
 /**
  * Command Parser for SockBot2.0
- * @module SockBot
+ * @module commands
  * @author Accalia
  * @license MIT
  */
@@ -13,12 +13,35 @@ const internals = {
     parseShortCommand: parseShortCommand,
     parseMentionCommand: parseMentionCommand
 };
+/**
+ * Parsed Command Data
+ *
+ * @name command
+ * @typedef {object}
+ * @param {string} input Raw Command Input
+ * @param {string} command Command name
+ * @param {string[]} args Command arguments
+ * @param {string} mention Mention text that was included in command
+ */
 
+/**
+ * Perpare the command parser
+ *
+ * Needs to be called to set the internals of the parser after reading config file.
+ *
+ * @param {completedCallback} callback Completion callback
+ */
 exports.prepareParser = function prepareParser(callback) {
     internals.mention = new RegExp('^@' + config.core.username + '\\s\\S{3,}(\\s|$)', 'i');
     callback(null);
 };
 
+/**
+ * Parse a short command from input line
+ *
+ * @param {string} line Input line to parse
+ * @returns {command} Parsed command
+ */
 function parseShortCommand(line) {
     if (!/^!\S{3,}(\s|$)/.test(line)) {
         return null;
@@ -33,6 +56,12 @@ function parseShortCommand(line) {
     };
 }
 
+/**
+ * Parse a mention command from input line
+ *
+ * @param {string} line Input line to parse
+ * @returns {command} Parsed command
+ */
 function parseMentionCommand(line) {
     if (!internals.mention.test(line)) {
         return null;
@@ -48,9 +77,18 @@ function parseMentionCommand(line) {
     };
 }
 
+/**
+ * Completion Callback
+ *
+ * @param {Exception} [err=null] Error encountered processing request
+ */
+function completedCallback(err) {} //eslint-disable-line handle-callback-err, no-unused-vars
+
 /* istanbul ignore else */
 if (typeof GLOBAL.describe === 'function') {
     //test is running
     exports.internals = internals;
-    exports.stubs = {};
+    exports.stubs = {
+        completedCallback: completedCallback
+    };
 }
