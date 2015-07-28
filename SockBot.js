f9fd2fe393bb9a81b722c0e135f9e1e31d829fb8
Accalia de Elementia <accalia.de.elementia@gmail.com>
2015-07-28 14:46:19 +0000

    change core to support new browser stuff

--- SockBot.js
+++ SockBot.js
@@ -11,12 +11,15 @@ exports.version = 'v2.0.0';
 
 
 
-const config = require('./config'),
-    browser = require('./browser');
+const config = require('./config');
+const browser = require('./browser')();
 
 /* istanbul ignore if */
 if (require.main === module) {
     config.loadConfiguration(process.argv[2], () => {
-        browser.login(() => console.log(arguments));
+        browser.login((err, user) => {
+            config.user = user;
+            console.log(err, config.user);
+        });
     });
 }
