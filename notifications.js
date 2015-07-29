d140c6f8f39054608c68dd3e1df2c02a1ecae618
Accalia de Elementia <accalia.de.elementia@gmail.com>
2015-07-29 11:37:44 +0000

    more work on notifications

--- notifications.js
+++ notifications.js
@@ -5,7 +5,9 @@
  * @author Accalia
  * @license MIT
  */
-const config = require('./config');
+const async = require('async');
+const config = require('./config'),
+    utils = require('./utils');
 const browser = require('./browser')();
 const internals = {
         notifyTypes: {
@@ -24,7 +26,8 @@ const internals = {
         }
     },
     privateFns = {
-        onNotificationMessage: onNotificationMessage
+        onNotificationMessage: onNotificationMessage,
+        handleTopicNotification: handleTopicNotification
     };
 
 exports.prepare = function prepare(events, callback) {
@@ -38,12 +41,61 @@ exports.pollNotifications = function pollNotifications(callback) {
         if (err) {
             return callback(err);
         }
+        notifications.filter((n) => !n.read).forEach((n) => {
+            n.type = internals.notifyTypes[n.notification_type];
+            if (!n.type) {
+                n.type = 'UNKNOWN';
+            }
+            if (n.topic_id) {
+                return privateFns.handleTopicNotification(n);
+            }
+            const handled = internals.events.emit('notification#' + n.type,
+                n, null, null);
+            if (!handled) {
+                utils.warn(n.type + ' notification #' + n.id + ' was not handled!');
+            }
+        });
+        callback(null);
     });
-    callback();
 };
+
+function handleTopicNotification(notification) {
+    async.parallel({
+        topic: (cb) => browser.getTopic(notification.topic_id, cb),
+        post: (cb) => browser.getPost(notification.data.original_post_id, cb)
+    }, (err, result) => {
+        if (err) {
+            return;
+        }
+        utils.filterIgnored(result.topic, result.post, (ignored) => {
+            if (!ignored) {
+                const handled = internals.events.emit('notification#' + notification.type, notification, result.topic,
+                    result.post);
+                if (!handled) {
+                    utils.warn(notification.type + ' notification #' + notification.id + ' was not handled!');
+                }
+            }
+        });
+    });
+}
 
 function onNotificationMessage(message) {
     if (message.unread_notifications > 0 || message.unread_private_messages > 0) {
         exports.pollNotifications(() => 0);
     }
+}
+
+/**
+ * Completion Callback
+ *
+ * @callback
+ * @name completionCallback
+ * @param {string|Error} err Filter Error state
+ */
+
+/* istanbul ignore else */
+if (typeof GLOBAL.describe === 'function') {
+    //test is running
+    exports.internals = internals;
+    exports.privateFns = privateFns;
 }
--- test/notificationsTest.js
+++ test/notificationsTest.js
@@ -8,6 +8,283 @@ const chai = require('chai'),
 chai.should();
 const expect = chai.expect;
 
-const notifications = require('../notifications');
+const notifications = require('../notifications'),
+    utils = require('../utils');
+const browser = require('../browser')();
 
-describe('notifications', () => {});
+describe('notifications', () => {
+    describe('exports', () => {
+        const fns = ['prepare', 'pollNotifications'],
+            objs = ['internals', 'privateFns'],
+            vals = [];
+        describe('should export expected functions:', () => {
+            fns.forEach((fn) => {
+                it(fn + '()', () => expect(notifications[fn]).to.be.a('function'));
+            });
+        });
+        describe('should export expected objects', () => {
+            objs.forEach((obj) => {
+                it(obj, () => expect(notifications[obj]).to.be.a('object'));
+            });
+        });
+        describe('should export expected values', () => {
+            vals.forEach((val) => {
+                it(val, () => notifications.should.have.key(val));
+            });
+        });
+        it('should export only expected keys', () => {
+            notifications.should.have.all.keys(fns.concat(objs, vals));
+        });
+    });
+    describe('internals', () => {
+        const fns = [],
+            objs = ['notifyTypes'],
+            vals = [];
+        describe('should export expected functions:', () => {
+            fns.forEach((fn) => {
+                it(fn + '()', () => expect(notifications.internals[fn]).to.be.a('function'));
+            });
+        });
+        describe('should export expected objects', () => {
+            objs.forEach((obj) => {
+                it(obj, () => expect(notifications.internals[obj]).to.be.a('object'));
+            });
+        });
+        describe('should export expected values', () => {
+            vals.forEach((val) => {
+                it(val, () => notifications.internals.should.have.key(val));
+            });
+        });
+        it('should export only expected keys', () => {
+            notifications.internals.should.have.all.keys(fns.concat(objs, vals));
+        });
+        describe('notifyTypes', () => {
+            const notifyTypes = notifications.internals.notifyTypes;
+            it('should only have expected notification types', () => {
+                const types = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
+                notifyTypes.should.have.all.keys(types);
+            });
+            const expectedMap = {
+                1: 'mentioned',
+                2: 'replied',
+                3: 'quoted',
+                4: 'edited',
+                5: 'liked',
+                6: 'private_message',
+                7: 'invited_to_private_message',
+                8: 'invitee_accepted',
+                9: 'posted',
+                10: 'moved_post',
+                11: 'linked',
+                12: 'granted_badge'
+            };
+            Object.keys(expectedMap).forEach((key) => {
+                it('should map #' + key + 'to type `' + expectedMap[key] + '`', () => {
+                    notifyTypes[key].should.equal(expectedMap[key]);
+                });
+            });
+        });
+    });
+    describe('privateFns', () => {
+        const fns = ['onNotificationMessage', 'handleTopicNotification'];
+        describe('should export expected functions:', () => {
+            fns.forEach((fn) => {
+                it(fn + '()', () => expect(notifications.privateFns[fn]).to.be.a('function'));
+            });
+        });
+        it('should export only expected keys', () => {
+            notifications.privateFns.should.have.all.keys(fns);
+        });
+        describe('onNotificationMessage()', () => {
+            const onNotificationMessage = notifications.privateFns.onNotificationMessage;
+            before(() => sinon.stub(notifications, 'pollNotifications'));
+            beforeEach(() => notifications.pollNotifications.reset());
+            it('should not poll for zero unread notifications', () => {
+                onNotificationMessage({
+                    'unread_notifications': 0
+                });
+                notifications.pollNotifications.called.should.be.false;
+            });
+            it('should poll for one unread notifications', () => {
+                onNotificationMessage({
+                    'unread_notifications': 1
+                });
+                notifications.pollNotifications.called.should.be.true;
+            });
+            it('should poll for many unread notifications', () => {
+                onNotificationMessage({
+                    'unread_notifications': 2 + Math.random() * 5000
+                });
+                notifications.pollNotifications.called.should.be.true;
+            });
+            it('should not poll for zero unread messages', () => {
+                onNotificationMessage({
+                    'unread_private_messages': 0
+                });
+                notifications.pollNotifications.called.should.be.false;
+            });
+            it('should poll for one unread messages', () => {
+                onNotificationMessage({
+                    'unread_private_messages': 1
+                });
+                notifications.pollNotifications.called.should.be.true;
+            });
+            it('should poll for many unread messages', () => {
+                onNotificationMessage({
+                    'unread_private_messages': 2 + Math.random() * 5000
+                });
+                notifications.pollNotifications.called.should.be.true;
+            });
+            it('should poll for one unread messages but no unread notifications', () => {
+                onNotificationMessage({
+                    'unread_notifications': 0,
+                    'unread_private_messages': 1
+                });
+                notifications.pollNotifications.called.should.be.true;
+            });
+            it('should poll for one unread notifications but no unread messages', () => {
+                onNotificationMessage({
+                    'unread_notifications': 1,
+                    'unread_private_messages': 0
+                });
+                notifications.pollNotifications.called.should.be.true;
+            });
+            it('should pass callback to pollNotifications', () => {
+                onNotificationMessage({
+                    'unread_notifications': 1,
+                    'unread_private_messages': 0
+                });
+                notifications.pollNotifications.firstCall.args[0].should.be.a('function');
+                notifications.pollNotifications.firstCall.args[0]().should.equal(0);
+            });
+            after(() => notifications.pollNotifications.restore());
+        });
+        describe('handleTopicNotification()', () => {
+            const handleTopicNotification = notifications.privateFns.handleTopicNotification,
+                notification = {
+                    id: Math.random(),
+                    'topic_id': 1234,
+                    type: 'someNotification',
+                    data: {
+                        'original_post_id': 5432
+                    }
+                },
+                events = {
+                    emit: sinon.stub()
+                };
+            before(() => {
+                notifications.internals.events = events;
+                sinon.stub(utils, 'filterIgnored');
+                sinon.stub(utils, 'warn');
+                sinon.stub(browser, 'getTopic');
+                sinon.stub(browser, 'getPost');
+            });
+            beforeEach(() => {
+                events.emit.reset();
+                utils.filterIgnored.reset();
+                utils.warn.reset();
+                browser.getTopic.reset();
+                browser.getPost.reset();
+            });
+            describe('getTopic/getPost subtasks', () => {
+                it('should get topic from topic_id in notification', () => {
+                    browser.getTopic.yields(null);
+                    browser.getPost.yields(null);
+                    handleTopicNotification(notification);
+                    browser.getTopic.calledWith(1234).should.be.true;
+                });
+                it('should get post from data.original_post_id in notification', () => {
+                    browser.getTopic.yields(null);
+                    browser.getPost.yields(null);
+                    handleTopicNotification(notification);
+                    browser.getPost.calledWith(5432).should.be.true;
+                });
+                it('should drop notification if getTopic errors', () => {
+                    browser.getTopic.yields('error');
+                    browser.getPost.yields(null);
+                    handleTopicNotification(notification);
+                    utils.filterIgnored.called.should.be.false;
+                });
+                it('should drop notification if getPost errors', () => {
+                    browser.getTopic.yields(null);
+                    browser.getPost.yields('error');
+                    handleTopicNotification(notification);
+                    utils.filterIgnored.called.should.be.false;
+                });
+                it('should drop notification if getTopic errors', () => {
+                    browser.getTopic.yields('error');
+                    browser.getPost.yields(null);
+                    handleTopicNotification(notification);
+                    utils.filterIgnored.called.should.be.false;
+                });
+            });
+            it('should pass topic and post to filterIgnored on success', () => {
+                const topic = {};
+                browser.getTopic.yields(null, topic);
+                browser.getPost.yields(null);
+                handleTopicNotification(notification);
+                utils.filterIgnored.called.should.be.true;
+                utils.filterIgnored.firstCall.args[0].should.equal(topic);
+            });
+            it('should pass topic and post to filterIgnored on success', () => {
+                const post = {};
+                browser.getTopic.yields(null);
+                browser.getPost.yields(null, post);
+                handleTopicNotification(notification);
+                utils.filterIgnored.called.should.be.true;
+                utils.filterIgnored.firstCall.args[1].should.equal(post);
+            });
+            it('should not emit event on ignored', () => {
+                browser.getTopic.yields(null);
+                browser.getPost.yields(null);
+                utils.filterIgnored.yields('ignore');
+                handleTopicNotification(notification);
+                events.emit.called.should.be.false;
+            });
+            it('should not emit event on ignored', () => {
+                browser.getTopic.yields(null);
+                browser.getPost.yields(null);
+                utils.filterIgnored.yields(null);
+                handleTopicNotification(notification);
+                events.emit.called.should.be.true;
+            });
+            it('should emit proper event on success', () => {
+                const topic = {},
+                    post = {};
+                browser.getTopic.yields(null, topic);
+                browser.getPost.yields(null, post);
+                utils.filterIgnored.yields(null);
+                handleTopicNotification(notification);
+                events.emit.calledWith('notification#someNotification', notification, topic, post).should.be.true;
+            });
+            it('should warn on unhandled notification', () => {
+                const topic = {},
+                    post = {};
+                browser.getTopic.yields(null, topic);
+                browser.getPost.yields(null, post);
+                utils.filterIgnored.yields(null);
+                handleTopicNotification(notification);
+                utils.warn.called.should.be.true;
+                utils.warn.firstCall.args[0].should.equal('someNotification notification #' +
+                    notification.id + ' was not handled!');
+            });
+            it('should not warn on handled notification', () => {
+                const topic = {},
+                    post = {};
+                browser.getTopic.yields(null, topic);
+                browser.getPost.yields(null, post);
+                utils.filterIgnored.yields(null);
+                events.emit.returns(true);
+                handleTopicNotification(notification);
+                utils.warn.called.should.be.false;
+            });
+            after(() => {
+                notifications.internals.events = null;
+                utils.filterIgnored.restore();
+                utils.warn.restore();
+                browser.getTopic.restore();
+                browser.getPost.restore();
+            });
+        });
+    });
+});
