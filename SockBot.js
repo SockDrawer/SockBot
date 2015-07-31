79be07246cf0d94c2d552de2a4529eba85c58309
Accalia de Elementia <accalia.de.elementia@gmail.com>
2015-07-31 13:47:26 +0000

    add comments/documentation to SockBot

--- SockBot.js
+++ SockBot.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node --harmony
 'use strict';
 /**
  * Main Module for SockBot2.0
@@ -20,6 +21,30 @@ const browser = require('./browser')();
 const plugins = [];
 let running = false;
 
+/**
+ * Prepared Callback
+ *
+ * @callback
+ * @name preparedCallback
+ * @param {string|Error} err Any Error encountered
+ * @param {external.events.SockEvents} events SockBot's internal event emitter with added helper functions
+ * @param {browser} pluginBrowser discourse communication class, will be logged into discourse once bot starts
+ */
+
+/**
+ * Completion Callback
+ *
+ * @callback
+ * @name completedCallback
+ * @param {string|Error} err Any Error encountered
+ */
+
+/**
+ * Prepare the bot for running
+ *
+ * @param {object|string} configuration Configuration to use. If string interpret as file path to read from
+ * @param {preparedCallback} callback Completion callback
+ */
 exports.prepare = function prepare(configuration, callback) {
     async.waterfall([(next) => {
         loadConfig(configuration, next);
@@ -34,6 +59,11 @@ exports.prepare = function prepare(configuration, callback) {
     }], callback);
 };
 
+/**
+ * Start the bot
+ *
+ * @param {completedCallback} callback Completion Callback
+ */
 exports.start = function (callback) {
     browser.login((err, user) => {
         if (err) {
@@ -50,30 +80,44 @@ exports.start = function (callback) {
         async.whilst(() => running, (next) => {
             notifications.pollNotifications(() => setTimeout(next), 5 * 60 * 1000);
         });
+        callback(null);
     });
-
 };
 
+/**
+ * Stop the event loop and signal plugins to stop
+ */
 exports.stop = function () {
     running = false;
     plugins.forEach((plugin) => plugin.stop());
 };
 
 
+/**
+ * Load module as plugin
+ *
+ * @param {string} module Module to require
+ * @returns {object} requested module
+ */
 function doPluginRequire(module) {
-    if (/\//.test(module)) {
-        return require(module);
-    }
     try {
+        // Look in plugins first
         return require('./plugins/' + module);
     } catch (e) {
+        // Error! check if it's ENOENT and try raw module
         if (/^Cannot find module/.test(e.message)) {
             return require(module);
         }
+        // Rethrow error if it wasn't ENOENT
         throw e;
     }
 }
 
+/**
+ * Prepare core EventEmitter as a SockEvents object
+ *
+ * @param {preparedCallback} callback Completion callback
+ */
 function prepareEvents(callback) {
     const events = new EventEmitter(),
         pluginBrowser = browser.setPlugins(),
@@ -94,6 +138,9 @@ function prepareEvents(callback) {
     });
 }
 
+/**
+ * Load plugins based on current configuration.
+ */
 function loadPlugins() {
     Object.keys(config.plugins).forEach((module) => {
         const plugin = doPluginRequire(module);
@@ -110,6 +157,12 @@ function loadPlugins() {
     });
 }
 
+/**
+ * Load configuration
+ *
+ * @param {string|object} cfg Configuration to use, if string load as filepath to configuration
+ * @param {completedCallback} callback CompletionCallback
+ */
 function loadConfig(cfg, callback) {
     if (typeof cfg === 'object' && typeof cfg.core === 'object' && typeof cfg.plugins === 'object') {
         config.core = cfg.core;
