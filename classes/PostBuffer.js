e9a4b43e78d6364a19bc44c114da3c690bc89778
RaceProUK <raceprouk@hotmail.co.uk>
2015-08-06 12:45:32 +0000

    Delinting some JSDoc

--- classes/PostBuffer.js
+++ classes/PostBuffer.js
@@ -55,7 +55,8 @@ module.exports = PostBuffer;
 /**
  * PostBuffer Callback
  *
- * @see {@link #classes.module_PostBuffer..PostBuffer+add|`add`} for more information about the members of key and values
+ * @see {@link #classes.module_PostBuffer..PostBuffer+add|`add`}
+ * for more information about the members of key and values
  *
  * @callback
  * @name postBufferCallback
