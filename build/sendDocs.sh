aae2e5190e60c1d27627983aa8dc5339e47dedcf
Accalia de Elementia <accalia.de.elementia@gmail.com>
2016-03-01 18:47:27 +0000

    whoopsies.... should probably switch to the right branch, correctly

--- build/sendDocs.sh
+++ build/sendDocs.sh
@@ -9,7 +9,7 @@ git config user.name "Travis-CI" || exit 1;
 git config user.email "Travis-CI@servercooties.com" || exit 2;
 git remote add github 'https://github.com/SockDrawer/SockBot.git' || exit 7;
 git fetch github || exit 10;
-git checkout github"$TRAVIS_BRANCH" || exit 3;
+git checkout "$TRAVIS_BRANCH" || exit 3;
 git pull github "$TRAVIS_BRANCH" || exit 4;
 git add -A "docs/api" || exit 5;
 git commit -m 'Automatic Push of Updated Documentation [ci skip]' || exit 6;
