5faf23a76a57859ae5f2ffdaebcee8b85091dcf4
Accalia de Elementia <accalia.de.elementia@gmail.com>
2016-03-01 17:23:05 +0000

    differentiate exit codes for debugging

--- build/sendDocs.sh
+++ build/sendDocs.sh
@@ -6,10 +6,10 @@ if [[ "$TRAVIS_JOB_NUMBER" != *.1 ]]; then
 fi
 
 git config user.name "Travis-CI" || exit 1;
-git config user.email "Travis-CI@servercooties.com" || exit 1;
-git checkout "$TRAVIS_BRANCH" || exit 1;
-git pull origin "$TRAVIS_BRANCH" || exit 1;
-git add "docs/api" || exit 1;
-git commit -m 'Automatic Push of Updated Documentation [ci skip]' || exit 1;
-git remote add github 'https://github.com/SockDrawer/SockBot.git' || exit 1;
+git config user.email "Travis-CI@servercooties.com" || exit 2;
+git checkout "$TRAVIS_BRANCH" || exit 3;
+git pull origin "$TRAVIS_BRANCH" || exit 4;
+git add "docs/api" || exit 5;
+git commit -m 'Automatic Push of Updated Documentation [ci skip]' || exit 6;
+git remote add github 'https://github.com/SockDrawer/SockBot.git' || exit 7;
 git push github HEAD
