b2f7f35b83fdf40e5e520a1fa97bbb1f833e4fc0
Accalia de Elementia <accalia.de.elementia@gmail.com>
2016-03-01 17:49:07 +0000

    i think i might have it?

--- build/sendDocs.sh
+++ build/sendDocs.sh
@@ -7,10 +7,11 @@ fi
 
 git config user.name "Travis-CI" || exit 1;
 git config user.email "Travis-CI@servercooties.com" || exit 2;
-git fetch || exit 10;
-git checkout "$TRAVIS_BRANCH" || exit 3;
-git pull origin "$TRAVIS_BRANCH" || exit 4;
+git remote add github 'https://github.com/SockDrawer/SockBot.git' || exit 7;
+git fetch github || exit 10;
+#//git checkout github"$TRAVIS_BRANCH" || exit 3;
+git pull github "$TRAVIS_BRANCH" || exit 4;
 git add "docs/api" || exit 5;
 git commit -m 'Automatic Push of Updated Documentation [ci skip]' || exit 6;
-git remote add github 'https://github.com/SockDrawer/SockBot.git' || exit 7;
+
 git push github HEAD
