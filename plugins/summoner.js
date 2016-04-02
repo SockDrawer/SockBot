f4ad49227e6e21f69a784691413c82f5ebd10862
Accalia de Elementia <accalia.de.elementia@gmail.com>
2016-04-02 14:50:43 +0000

    listen to the right event + documentation

--- plugins/summoner.js
+++ plugins/summoner.js
@@ -7,12 +7,21 @@ module.exports = function summoner(forum, config) {
         'Yes master %name%, I shall appear as summoned.',
         'Yes mistress %name%, I shall appear as summoned.'
     ];
+
     if (Array.isArray(config) && config.length > 0) {
         messages = config;
     } else if (Array.isArray(config.messages) && config.messages.length > 0) {
         messages = config.messages;
     }
 
+    /**
+     * Handle a mention notification.
+     *
+     * Choose a random message and reply with it
+     *
+     * @param {Notification} notification Notification event to handle
+     * @returns {Promise} Resolves when event is processed
+     */
     function handler(notification) {
         return Promise.all([notification.getPost(),
                 notification.getUser()
@@ -21,28 +30,35 @@ module.exports = function summoner(forum, config) {
                 const post = data[0],
                     user = data[1],
                     index = Math.floor(Math.random() * messages.length);
-                return post.reply(messages[index].replace(/%(\w+)%/g, (_, key) => {
+                const message = messages[index].replace(/%(\w+)%/g, (_, key) => {
                     let value = user[key] || `%${key}%`;
                     if (typeof value !== 'string') {
                         value = `%${key}%`;
                     }
                     return value;
-                }));
-            });
+                });
+                return post.reply(message);
+            }).catch((err) => forum.emit('error', err));
     }
 
+    /**
+     * Activate the plugin
+     */
     function activate() {
-        forum.on('notification.mention', handler);
+        forum.on('notification:mention', handler);
     }
 
+    /**
+     * Deactivate the plugin
+     */
     function deactivate() {
-        forum.off('notification.mention', handler);
+        forum.off('notification:mention', handler);
     }
 
     return {
         activate: activate,
         deactivate: deactivate,
         handler: handler,
-        messages: mes
+        messages: messages
     };
 };
