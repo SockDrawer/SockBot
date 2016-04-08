38667a08027088791c2f7baa002555f805edb3c6
Accalia Elementia <Accalia.de.elementia@gmail.com>
2016-04-08 14:58:32 -0400

    test build script (#299)
    * test build script 1
    
    * test build script 2
    
    * update build scripts

--- build/sendDocs.sh
+++ build/sendDocs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ "$TRAVIS_PULL_REQUEST" = "true" ]]; then
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
     echo "$TRAVIS_PULL_REQUEST: Not pushing docs for pull request"
     exit 0; # nothing to do here, it's a PR
 fi
@@ -17,6 +17,6 @@ git fetch github || exit 10;
 git checkout "$TRAVIS_BRANCH" || exit 3;
 git pull github "$TRAVIS_BRANCH" || exit 4;
 git add -A "docs/api" || exit 5;
-git commit -m 'Automatic Push of Updated Documentation [ci skip]' || exit 6;
+git commit -m 'Automatic Push of Updated Documentation [ci skip]' || exit 0;
 
 git push github HEAD
