d1398ab12de23b4a91ec85e26a1dbdc9153fb19b
Accalia de Elementia <accalia.de.elementia@gmail.com>
2016-04-21 11:37:36 +0000

    update echo command to support chats as well.

--- plugins/echo.js
+++ plugins/echo.js
@@ -22,13 +22,9 @@ exports.plugin = function plugin(forum) {
      * @returns {Promise} Resolves when processing is complete
      */
     function echo(command) {
-        return Promise.all([
-            command.getPost(),
-            command.getUser()
-        ]).then((data) => {
-            const post = data[0];
-            const user = data[1];
-            const content = (post.content || '').split('\n').map((line) => `> ${line}`);
+        return command.getUser()
+        .then((user) => {
+            const content = (command.parent.text || '').split('\n').map((line) => `> ${line}`);
             content.unshift(`@${user.username} said:`);
             command.reply(content.join('\n'));
         });
